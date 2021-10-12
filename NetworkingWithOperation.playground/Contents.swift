66b92262e8d9c704c7116143ee626ade577a5284
rhodstar <rhodfra@gmail.com>
2021-10-12 11:50:34 -0500

    Fix typo

--- NetworkingWithOperation.playground/Contents.swift
+++ NetworkingWithOperation.playground/Contents.swift
@@ -11,36 +11,24 @@ struct DetailedImage {
     let metadata: ImageMetadata
 }
 
-enum ImageDownloadError: Error {
-    case badImage
-    case invalidMetadata
-}
-
 let BASEURL = "https://www.andyibanez.com/fairesepages.github.io/tutorials/async-await/part1"
 
 //MARK:- Defining FETCH functions
-func downloadImageAndMetadata(
-    imageNumber: Int,
-    completionHandler: @escaping (_ image: DetailedImage?, _ error: Error?) -> Void
-) {
+func downloadImage( imageNumber: Int) -> UIImage? {
     let imageUrl = URL(string: BASEURL+"/\(imageNumber).png")!
-    let imageTask = URLSession.shared.dataTask(with: imageUrl) { data, response, error in
-        guard let data = data, let image = UIImage(data: data), (response as? HTTPURLResponse)?.statusCode == 200 else {
-            completionHandler(nil, ImageDownloadError.badImage)
-            return
-        }
-        let metadataUrl = URL(string: BASEURL+"/\(imageNumber).json")!
-        let metadataTask = URLSession.shared.dataTask(with: metadataUrl) { data, response, error in
-            guard let data = data, let metadata = try? JSONDecoder().decode(ImageMetadata.self, from: data),  (response as? HTTPURLResponse)?.statusCode == 200 else {
-                completionHandler(nil, ImageDownloadError.invalidMetadata)
-                return
-            }
-            let detailedImage = DetailedImage(image: image, metadata: metadata)
-            completionHandler(detailedImage, nil)
-        }
-        metadataTask.resume()
+    if let data = try? Data(contentsOf: imageUrl) {
+        return UIImage(data: data)!
     }
-    imageTask.resume()
+    return nil
+}
+
+func downloadMetadata(imageNumber: Int) -> ImageMetadata! {
+    let metaDataUrl = URL(string: BASEURL+"/\(imageNumber).json")!
+    if let data = try? Data(contentsOf: metaDataUrl) {
+        let metadata = try? JSONDecoder().decode(ImageMetadata.self, from: data)
+        return metadata
+    }
+    return nil
 }
 
 // MARK:- Fetching information with NSOperations|
@@ -48,14 +36,20 @@ let queue = OperationQueue()
 // We will be using main operation Queue to update UI Components
 let mainQueue = OperationQueue.main
 
-let operation = BlockOperation(block: {
-    print("Started download")
-    downloadImageAndMetadata(imageNumber: 1) { imageDetail, error in
-        print("Finished downloaed")
-        mainQueue.addOperation {
-            print("Updating UI components in main thread")
-        }
-    }
+var myImage: UIImage?
+var metadata: ImageMetadata?
+
+let operationForImage = BlockOperation(block: {
+    print("Started image download")
+    myImage = downloadImage(imageNumber: 1)
+    print("Finished image download")
 })
 
-queue.addOperation(operation)
+let operationForMetadata = BlockOperation(block: {
+    print("Started download metadata")
+    metadata = downloadMetadata(imageNumber: 1)
+    print("Finished download metadata")
+})
+
+queue.addOperation(operationForImage)
+queue.addOperation(operationForMetadata)
